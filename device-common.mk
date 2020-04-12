e98a2a7dead24f4c599436f18f3ed06471619e63
Alessandro <ales.astone@gmail.com>
2020-04-12 14:48:55 +0200

    grouper: import {wpa,p2p}_supplicant_overlay from config-bcm.mk
    Change-Id: I693a2d49948ddfcd3b1f47005693ad67133b8d8d

--- device-common.mk
+++ device-common.mk
@@ -119,6 +119,10 @@ PRODUCT_PACKAGES += \
     dhcpcd.conf \
     wpa_supplicant \
     wpa_supplicant.conf
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf    
 
 PRODUCT_PACKAGES += \
     libhealthd.tegra3 \
